941c21bd010a5e0ecd5c631702299c0a24df0702
brunogsiq <brunogsiq@gmail.com>
2025-05-08 14:04:32 -0300

    new config.js

--- cypress.config.js
+++ cypress.config.js
@@ -20,3 +20,31 @@ module.exports = defineConfig({
     ],
   },
 });
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig(
+{
+    viewportWidth: 1300,
+    viewportHeight: 800,
+    video: true,
+    chromeWebSecurity: false,
+    reporter: "mochawesome",
+    reporterOptions: {
+        reportDir: 'cypress/report',
+        overwrite: true,
+        html: true,
+        json: false,
+        timestamp: 'dd-mm-yyyy_HH-MM-ss'
+    },
+    e2e: {
+        defaultCommandTimeout: 9000,
+        experimentalRunAllSpecs: true,
+        hideXHRInCommandLog: true,
+        baseUrl: "https://webdriveruniversity.com/index.html",
+        //Aqui ficarão todos os meus testes:
+        setupNodeEvents(on, config) {
+          // implement node event listeners here
+        },
+    },
+});
